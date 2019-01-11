1c11ac50c4fed885ecfb1709c6a95373766de679
İsmail Hakkı Yadigar <ismail.yadigar@cutesafe.com>
2019-01-11 11:52:27 +0300

    Firebase configuration added.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,26 @@ import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
 
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyDC7HX2ouENPlk8rIK1eGfOoq0IwTnRFf0',
+      authDomain: 'employeemanager-d9b24.firebaseapp.com',
+      databaseURL: 'https://employeemanager-d9b24.firebaseio.com',
+      projectId: 'employeemanager-d9b24',
+      storageBucket: 'employeemanager-d9b24.appspot.com',
+      messagingSenderId: '134359240343'
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
-      <Provider>
+      <Provider store={createStore(reducers)}>
         <View>
           <Text>
             Merhaba
