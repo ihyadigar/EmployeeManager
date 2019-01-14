ddc6b427ad27d23d51c2f3d10721d966d4c104e5
İsmail Hakkı Yadigar <ismail.yadigar@cutesafe.com>
2019-01-14 10:25:34 +0300

    Firebase api keys moved to the hidden config file.

--- src/App.js
+++ src/App.js
@@ -3,21 +3,14 @@ import { Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import firebase from 'firebase';
+import firebaseConfig from './utils/config';
 import reducers from './reducers';
 import LoginForm from './components/LoginForm';
 
 
 class App extends Component {
   componentWillMount() {
-    const config = {
-      apiKey: 'AIzaSyDC7HX2ouENPlk8rIK1eGfOoq0IwTnRFf0',
-      authDomain: 'employeemanager-d9b24.firebaseapp.com',
-      databaseURL: 'https://employeemanager-d9b24.firebaseio.com',
-      projectId: 'employeemanager-d9b24',
-      storageBucket: 'employeemanager-d9b24.appspot.com',
-      messagingSenderId: '134359240343'
-    };
-    firebase.initializeApp(config);
+    firebase.initializeApp(firebaseConfig);
   }
 
   render() {
